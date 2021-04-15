12226b27386e17e98c02c195d19c352c480a4946
developer-jdsl <78074341+developer-jdsl@users.noreply.github.com>
2021-04-15 11:31:11 +0530

    add conifg and db file to gitignore

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://162.215.253.45/gurpal/';
+$config['base_url'] = 'http://localhost/gurpal/';
 
 /*
 |--------------------------------------------------------------------------
